c33eec3e342a4f401c7e6a20842c682aad8c94c5
megamayoy <mega_mayoy@hotmail.com>
2019-01-01 06:43:37 +0200

    create a templates folder inside the app folder instead of a main templates folder

--- MuliUserBlog_Django/settings.py
+++ MuliUserBlog_Django/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MuliUserBlog_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ TEMPLATES_DIR ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
