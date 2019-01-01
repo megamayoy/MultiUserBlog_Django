dcae8a587712892670955cc0452e5fc558244eb1
megamayoy <mega_mayoy@hotmail.com>
2019-01-01 12:18:13 +0200

    use Post model to show created posts in home.html

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 # Create your views here.
 
 
 def home(request):
 
-    return render(request,'blog/home.html')
+    posts = Post.objects.all()
+    return render(request,'blog/home.html',context = {'posts' : posts})
 
 def about(request):
     return render(request,"blog/about.html")
