9d6fa1fe9c961d707a37461af57a580a11c61fe2
megamayoy <mega_mayoy@hotmail.com>
2019-01-02 23:51:00 +0200

    make the email field required in the registration form

--- user_app/forms.py
+++ user_app/forms.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 class RegistrationForm(UserCreationForm):
 
 
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ['username','email','password1' ,'password2']
