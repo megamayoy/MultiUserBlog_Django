d672d45dcf42a385dbd01e1c582ee168f85eb07b
megamayoy <mega_mayoy@hotmail.com>
2019-01-03 00:10:54 +0200

    overriding the constructor of registration form to remove help text

--- user_app/forms.py
+++ user_app/forms.py
@@ -10,6 +10,14 @@ class RegistrationForm(UserCreationForm):
 
     email = forms.EmailField(required=True)
 
+
+    def __init__(self, *args, **kwargs):
+
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = User
         fields = ['username','email','password1' ,'password2']
