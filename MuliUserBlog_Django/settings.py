a305983cf62d9c01d76a211d57cc0e83c2ef91c8
megamayoy <mega_mayoy@hotmail.com>
2019-01-03 12:42:15 +0200

    enable user login & logout
    using the out of the box LoginView and LogoutView
    
    -declared the LOGIN_REDIRECT_URL constant
    to redirect the user to the home page when
    they login.
    -add the href in login  & logout buttons in
    navigation bar

--- MuliUserBlog_Django/settings.py
+++ MuliUserBlog_Django/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+
+#login redirect
+
+LOGIN_REDIRECT_URL = 'blog:blog-home'
--- user_app/urls.py
+++ user_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from user_app import views
 
-
+from django.contrib.auth import views as auth_views
 
 app_name = 'user_app'
 
 urlpatterns = [
 
     path('register/', views.register_user, name = 'user-registration'),
+    path('login/',auth_views.LoginView.as_view(template_name = "user_app/login.html"),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user_app/logout.html'),name ='logout'),
 
 ]
