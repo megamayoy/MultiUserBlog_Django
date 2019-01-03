c0e3bbdbf85a2eb5ec6361d1363f9fe4b70e9f94
megamayoy <mega_mayoy@hotmail.com>
2019-01-04 00:41:21 +0200

    enable using media files to show user profile pictures

--- user_app/models.py
+++ user_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User,on_delete = models.CASCADE)
-    profile_pic = models.ImageField(default = 'default_pic' , upload_to = "profile_pics")
+    profile_pic = models.ImageField(default = 'default.jpg' , upload_to = "profile_pics")
 
 
     def __str__(self):
--- user_app/urls.py
+++ user_app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('register/', views.register_user, name = 'user-registration'),
     path('login/',auth_views.LoginView.as_view(template_name = "user_app/login.html"),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user_app/logout.html'),name ='logout'),
-    path('profile/',views.user_profile,name = 'profile')
+    path('profile/',views.user_profile,name = 'profile'),
 
 ]
