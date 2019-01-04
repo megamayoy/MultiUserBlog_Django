209a9b35a0369b1ab682bac2fb0c623be30b0e1c
megamayoy <mega_mayoy@hotmail.com>
2019-01-04 21:35:22 +0200

    override the userprofile's save method to resize large profile pictures

--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -10,3 +11,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username + " profile"
+
+
+    def save(self):
+
+        super().save()
+        img = Image.open(self.profile_pic.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
