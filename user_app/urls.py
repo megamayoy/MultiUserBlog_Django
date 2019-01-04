cd61e86ce973ea1ce62b531893f1c004cc1a2f59
megamayoy <mega_mayoy@hotmail.com>
2019-01-04 20:35:59 +0200

    create and use  a route for changing user profile info

--- user_app/urls.py
+++ user_app/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name = "user_app/login.html"),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user_app/logout.html'),name ='logout'),
     path('profile/',views.user_profile,name = 'profile'),
+    path('changeinfo/',views.change_user_profile,name = 'changeinfo'),
 
 ]
