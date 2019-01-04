d998fd56779271d7ea49b2f8f9a066ad57609e2e
megamayoy <mega_mayoy@hotmail.com>
2019-01-04 19:59:56 +0200

    create forms to change user and profile settings

--- user_app/forms.py
+++ user_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from user_app.models import UserProfile
 
 
 
@@ -21,4 +22,26 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1' ,'password2']
+
+
+class ChangeProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_pic']
+
+        labels = {
+
+            'profile_pic' : "profile picture"
+        }
+
+
+class ChangeUserstngsForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username' , 'email']
+
+
+
 
