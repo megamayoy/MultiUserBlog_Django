5cfb50d1d02f4f018c16b313b0c29b6edb9b6801
megamayoy <mega_mayoy@hotmail.com>
2019-01-05 09:25:49 +0200

    use a DetailView to show posts' details

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
 
     path('',views.PostListView.as_view(), name = 'blog-home'),
-    path('about/',views.about,name = 'blog-about')
+    path('about/',views.about,name = 'blog-about'),
+    path('post/<int:pk>/',views.PostDetailView.as_view(), name = 'post-detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
 
@@ -14,7 +14,10 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+class PostDetailView(DetailView):
 
+    model = Post
+    context_object_name = 'post'
 
 def about(request):
     return render(request,"blog/about.html")
