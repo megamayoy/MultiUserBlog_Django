5d558f38beefdd21e57715c8870116fbbbf4d0c3
megamayoy <mega_mayoy@hotmail.com>
2019-01-05 10:31:14 +0200

    enable creating and updating posts

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('',views.PostListView.as_view(), name = 'blog-home'),
     path('about/',views.about,name = 'blog-about'),
     path('post/<int:pk>/',views.PostDetailView.as_view(), name = 'post-detail'),
+    path('post/create/',views.PostCreateView.as_view(),name = 'post-create'),
+    path('post/update/<int:pk>/',views.PostUpdatView.as_view(),name = 'post-update'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -18,6 +19,27 @@ class PostDetailView(DetailView):
 
     model = Post
     context_object_name = 'post'
+
+class PostCreateView(CreateView):
+
+    model = Post
+    fields = ['title','content']
+    success_url = reverse_lazy('blog:blog-home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdatView(UpdateView):
+
+    model = Post
+    fields = ['title', 'content']
+    success_url = reverse_lazy('blog:blog-home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request,"blog/about.html")
