ab65e9b56b3f2e8a601815272214c6edaae4fa79
megamayoy <mega_mayoy@hotmail.com>
2019-01-07 08:27:23 +0200

    create reset password forms and urls

--- user_app/urls.py
+++ user_app/urls.py
@@ -14,10 +14,17 @@ urlpatterns = [
     path('password_change/',auth_views.PasswordChangeView.as_view(template_name = 'user_app/password_change_form.html',
                                                                   success_url = reverse_lazy('user_app:logout')), name = 'password_change'),
 
-    path('password_reset/',auth_views.PasswordResetView.as_view(template_name = "user_app/password_reset_form.html "), name = 'password_reset'),
-    path('password_reset_done/',auth_views.PasswordResetDoneView(template_name = "user_app/password_reset_done.html").as_view(),name = 'password_reset_done'),
+    #if user forgets password
 
-    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = 'user_app/password_reset_confirm.html'),
-            success_url = reverse_lazy("user_app:login"), name = 'password_reset_confirm'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name = "user_app/password_reset_form.html",
+                                             email_template_name = "user_app/password_reset_email.html" ), name = 'password_reset'),
 
+
+    path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(template_name = "user_app/password_reset_done.html"),
+                                              name = 'password_reset_done'),
+
+
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = 'user_app/password_reset_confirm.html',
+                                              success_url = reverse_lazy("user_app:login")), name = 'password_reset_confirm'),
+    ##
 ]
