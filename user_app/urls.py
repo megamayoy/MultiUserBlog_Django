2ddf43ae5e2ef4d92425e053ed74fdd85b17d97a
megamayoy <mega_mayoy@hotmail.com>
2019-01-07 05:36:05 +0200

    enable changing user's password

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from user_app import views
 
 from django.contrib.auth import views as auth_views
-
+from django.urls import reverse_lazy
 app_name = 'user_app'
 
 urlpatterns = [
@@ -12,5 +12,7 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='user_app/logout.html'),name ='logout'),
     path('profile/',views.user_profile,name = 'profile'),
     path('changeinfo/',views.change_user_profile,name = 'changeinfo'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name = 'user_app/password_change_form.html',
+                                                                  success_url = reverse_lazy('user_app:logout')), name = 'password_change'),
 
 ]
