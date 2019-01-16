5c791f402c1d20d30abfd296e08015d728a4c4dc
megamayoy <mega_mayoy@hotmail.com>
2019-01-16 10:16:32 +0200

    fix password reset part
    a success_url has to be added in password_reset route because I use URl-namespacing
     and that was preventing calling password_reset_done route automatically
    after sending the password reset email.

--- user_app/urls.py
+++ user_app/urls.py
@@ -17,11 +17,12 @@ urlpatterns = [
     #if user forgets password
 
     path('password_reset/',auth_views.PasswordResetView.as_view(template_name = "user_app/password_reset_form.html",
-                                             email_template_name = "user_app/password_reset_email.html" ), name = 'password_reset'),
+                                          success_url = reverse_lazy("user_app:password_reset_done"),email_template_name = "user_app/password_reset_email.html" ),
+                                        name = 'password_reset'),
 
 
     path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(template_name = "user_app/password_reset_done.html"),
-                                              name = 'password_reset_done'),
+         name ='password_reset_done'),
 
 
     path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = 'user_app/password_reset_confirm.html',
