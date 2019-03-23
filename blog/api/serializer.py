a3fc4640bad492184919abe219023a0e6a51bb53
megamayoy <mega_mayoy@hotmail.com>
2019-03-23 21:38:45 +0200

    create User serialzier

--- blog/api/serializer.py
+++ blog/api/serializer.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from blog.models import Post
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["username","email"]
+
 
 class PostSerializer(serializers.ModelSerializer):
 
+    author = UserSerializer()
     class Meta:
         model = Post
         fields = ['id','title','content','author']
