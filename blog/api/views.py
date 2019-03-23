0f529d3fdf255e5d1df1432beab5c7a49f5f2617
megamayoy <mega_mayoy@hotmail.com>
2019-03-23 21:08:09 +0200

    add permission classes in RUD API view

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import RetrieveUpdateDestroyAPIView,ListAPIView,CreateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from blog.api.permissions import IsOwnerOrReadOnly
 from blog.models import Post
 from blog.api.serializer import PostSerializer
+
 
 
 class PostListAPIView(ListAPIView):
@@ -20,5 +23,4 @@ class PostRUDAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     lookup_field = 'pk'
     serializer_class = PostSerializer
-
-
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
