52b811d65163903fed2fde44d075ebcaf91648c8
megamayoy <mega_mayoy@hotmail.com>
2019-03-23 19:35:06 +0200

    create a url conf for the post list api view

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('post/<int:pk>/', views.PostRUDAPIView.as_view(), name='post-rud'),
+    path('post/list/',views.PostListAPIView.as_view(),name = 'post-list')
 
 ]
