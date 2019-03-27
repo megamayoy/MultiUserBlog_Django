0ee81ba48d5cfa79ba79237ee6f72eb533d7d290
megamayoy <mega_mayoy@hotmail.com>
2019-03-27 21:47:39 +0200

    fix a typo

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 
     path('post/<int:pk>/', views.PostRUDAPIView.as_view(), name='post-rud'),
     path('post/list/',views.PostListAPIView.as_view(),name = 'post-list'),
-    path('post/create/',views.PostCreateAPIView.as_view(),name = 'post-create0')
+    path('post/create/',views.PostCreateAPIView.as_view(),name = 'post-create')
 
 ]
